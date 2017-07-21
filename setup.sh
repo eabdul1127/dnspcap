8a70a9beb4824db1197825ac9cfb3248d71b546b
eabdul1127 <eabdul1127@gmail.com>
2017-07-21 15:02:33 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ npm install pm2 -g
 mv ./node_modules /usr/sbin/
 mv ./dnspcap.js /usr/sbin
 pm2 startup
-pm2 start /usr/sbin/dnspcap.js eno1 &
-pm2 start /usr/sbin/dnspcap.js eno2 &
-pm2 start /usr/sbin/dnspcap.js eno3 &
+pm2 start /usr/sbin/dnspcap.js -- eno1
+pm2 start /usr/sbin/dnspcap.js -- eno2
+pm2 start /usr/sbin/dnspcap.js -- eno3
 pm2 save
