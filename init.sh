15a141abff895516d8c5babc7a65a07fcb11b605
eabdul1127 <eabdul1127@gmail.com>
2017-07-21 12:59:09 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ apt-get update
 apt-get install ansible
 echo "exports.secret_key = 'Compassys';" > /etc/dnscap.js
 ansible-playbook ansible.yml
-scp usr/sbin/dnscap/dnscap.js ..
+scp usr/sbin/dnspcap/dnspcap.js ..
 pm2 startup
 pm2 start /usr/sbin/dnspcap.js eno1 &
 pm2 start /usr/sbin/dnspcap.js eno2 &
